629fc374941a2ed57a2ef78962fd423563392c28
Hannah Tomasini <hatomasini@edu.aau.at>
2024-03-06 20:24:42 +0100

    2.2 Implement a second button that sorts the matriculation number ascending and without primes.

--- app/src/main/java/com/example/einzelbeispiel/MainActivity.java
+++ app/src/main/java/com/example/einzelbeispiel/MainActivity.java
@@ -14,6 +14,9 @@ import java.io.BufferedWriter;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 import java.net.Socket;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -28,13 +31,21 @@ public class MainActivity extends AppCompatActivity {
 
         matNr = (EditText)findViewById(R.id.editText);
         server = (TextView)findViewById(R.id.textView3);
-        Button button = findViewById(R.id.button);
+        Button button = findViewById(R.id.buttonSend);
+        Button button2 = findViewById(R.id.buttonCalc);
 
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                String matrikelnummer = matNr.getText().toString();
+                String matrikelnummer = matNr.getText().toString().trim();
                 contactServer(matrikelnummer);
+            }
+        });
+        button2.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                String matrikelnummer = matNr.getText().toString().trim();
+                server.setText(sortWithoutPrimes(matrikelnummer));
             }
         });
 
@@ -70,5 +81,27 @@ public class MainActivity extends AppCompatActivity {
             }
         }).start();
 
+    }
+    private String sortWithoutPrimes(String matrikelnummer){
+        String input = matrikelnummer;
+        List<Integer> number = new ArrayList<>();
+        for (int i = 0; i < input.length(); i++){
+            if(!isPrime(Character.getNumericValue(matrikelnummer.charAt(i)))) {
+                number.add(Character.getNumericValue(matrikelnummer.charAt(i)));
+            }
+        }
+        Collections.sort(number);
+        return number.toString();
+    }
+    private boolean isPrime(int num){
+        if (num <= 1) {
+            return false;
+        }
+        for (int i = 2; i <= Math.sqrt(num); i++) {
+            if (num % i == 0) {
+                return false;
+            }
+        }
+        return true;
     }
 }
